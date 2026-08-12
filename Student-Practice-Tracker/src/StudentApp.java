4e27b41217d915e4f01a5545c3e3e593d6bb1214
Biku2004 <bikash.behera24.04.2004@gmail.com>
2026-08-12 20:57:54 +0530

    created scanner, object creating and scanners

--- Student-Practice-Tracker/src/StudentApp.java
+++ Student-Practice-Tracker/src/StudentApp.java
@@ -1,4 +1,63 @@
+import java.util.Scanner;
+
 public class StudentApp {
 
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Student student = new Student();
+
+        System.out.println("============================");
+        System.out.println("  STUDENT PRACTICE TRACKER");
+        System.out.println("============================");
+        System.out.println();
+
+        System.out.println("Enter Student Name:");
+        String name = scanner.nextLine();
+
+
+        System.out.println();
+        System.out.println("Enter Course Name:");
+        String courseName = scanner.nextLine();
+
+        System.out.println();
+        System.out.println("Enter Completed Topics:");
+        int completedTopics = scanner.nextInt();
+
+        System.out.println();
+        System.out.println("Enter Questions Solved:");
+        int questionsSolved = scanner.nextInt();
+
+        System.out.println();
+        student.setDetails(name, courseName);
+        student.setProgress(completedTopics, questionsSolved);
+
+        boolean keepRunning = true;
+
+        System.out.println();
+        while (keepRunning) {
+            
+            
+            System.out.println
+            ("""
+                ============================
+                    MENU
+                ============================
+                1. View Student Details
+                2. View Progress
+                3. Add Completed Topics
+                4. Add Solved Questions
+                5. Exit
+            """);
+            
+
+
+            System.out.println();
+            System.out.println("Enter your choice:");
+            int choice = scanner.nextInt();
+
+        }
+        
+        scanner.close();
+    }
 }
