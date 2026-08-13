5391805f4132f8a7768e97cdb27c981065835f8e
Biku2004 <bikash.behera24.04.2004@gmail.com>
2026-08-14 00:41:39 +0530

    Execution of Program and adding output

--- Student-Practice-Tracker/src/StudentApp.java
+++ Student-Practice-Tracker/src/StudentApp.java
@@ -7,25 +7,27 @@ public class StudentApp {
         Scanner scanner = new Scanner(System.in);
         Student student = new Student();
 
-        System.out.println("============================");
-        System.out.println("  STUDENT PRACTICE TRACKER");
-        System.out.println("============================");
+        System.out.println("""
+                ============================
+                STUDENT PRACTICE TRACKER
+                ============================
+                """);
         System.out.println();
 
-        System.out.println("Enter Student Name:");
+        System.out.println("Enter Student Name: ");
         String name = scanner.nextLine();
 
 
         System.out.println();
-        System.out.println("Enter Course Name:");
+        System.out.println("Enter Course Name: ");
         String courseName = scanner.nextLine();
 
         System.out.println();
-        System.out.println("Enter Completed Topics:");
+        System.out.println("Enter Completed Topics: ");
         int completedTopics = scanner.nextInt();
 
         System.out.println();
-        System.out.println("Enter Questions Solved:");
+        System.out.println("Enter Questions Solved: ");
         int questionsSolved = scanner.nextInt();
 
         System.out.println();
@@ -53,9 +55,52 @@ public class StudentApp {
 
 
             System.out.println();
-            System.out.println("Enter your choice:");
+            System.out.println("Enter your choice: ");
             int choice = scanner.nextInt();
 
+            // choice logic
+            if (choice == 1) {
+                System.out.println();
+                student.displayDetails();
+            } 
+
+            else if (choice == 2) {
+                System.out.println();
+                student.displayProgress();
+            } 
+
+            else if (choice == 3) {
+                System.out.println();
+
+                System.out.println("Enter New Completed Topics: ");
+                int newTopics = scanner.nextInt();
+
+                System.out.println();
+                student.addTopics(newTopics);
+            } 
+
+            else if (choice == 4) {
+                System.out.println();
+
+                System.out.println("Enter New Questions Solved: ");
+                int newQuestions = scanner.nextInt();
+
+                System.out.println();
+
+                student.addQuestions(newQuestions);
+            } 
+
+            else if (choice == 5) {
+                System.out.println();
+                System.out.println("Thank you for using Student Practice Tracker");
+                System.out.println("Keep Practicing!");
+                keepRunning = false;
+            } 
+
+            else {
+                System.out.println("Invalid choice");
+                System.out.println("Please enter a value between 1 and 5");
+            }
         }
         
         scanner.close();
